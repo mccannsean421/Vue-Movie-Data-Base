81f61412f0249a66d4f265a900218b7b3270e3b1
mccannsean421 <mccannsean421@gmail.com>
2018-08-10 13:32:21 +0100

    completed video 11: looping in vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <Header v-bind:title="title"/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header v-bind:title="title" />
+    <ul>
+      <li v-for="(item, index) in movies">{{ index + 1 }} {{ item }}</li>
+      <li v-for="actor in actors"> {{ actor.name }}</li>
+
+      <!-- LOOP THROUGH AN OBJECT -->
+      <li v-for="(value, key) in profile"> {{key}}: {{ value }} </li>
+
+      <!-- LOOPING OVER A COMPONENT 
+      <Header 
+        :key="actor.id"
+        v-for="actor in actors"
+        :title="actor.name"
+      /--> 
+    </ul>
   </div>
 </template>
 
@@ -15,7 +28,21 @@ export default {
   data() {
     return {
       message: 'test',
-      title: 'Vue movie DB'
+      title: 'Vue movie DB',
+      movies: [
+        'Terminator',
+        'Gremlins',
+        'Ghostbusters'
+      ],
+      actors: [
+        { id: 1, name: 'Bill Murray'},
+        { id: 2, name: 'Samuel L Jackson'}
+      ],
+      profile: {
+        name: 'James Cameron',
+        age: 'I dunno 70?',
+        job: 'Director'
+      }
     }
   },
   components: {
