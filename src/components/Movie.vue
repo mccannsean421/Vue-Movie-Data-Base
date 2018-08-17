b0ebdb56a918fce3569502145715dc0ecad273f9
mccannsean421 <mccannsean421@gmail.com>
2018-08-17 16:12:29 +0100

    completed video 16: creating a movies list

---
+++ src/components/Movie.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img :src="movie.posterPath" :alt="movie.title" /> 
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['movie']
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,19 +1,24 @@
 <template>
     <div>
         Movies list
-        <ul>
-            <li>{{ movie.original_title }}</li>
-        </ul>
-    </div>
+        
+            <div v-for="movie in movies"><Movie v-bind:movie="movie" /></div>
+        
+    </div>  
 </template>
 
 <script>
+    import Movie from './Movie.vue';
+
     export default {
         name: "MoviesList",
         data: function() {
             return {
-                movie: {}
+                movies: []
             }
+        },
+        components: {
+            Movie
         },
         created: function() {
             this.fetchData()
@@ -21,9 +26,10 @@
         methods: {
             fetchData: async function() {
                 try {
-                    const res = await fetch('https://api.themoviedb.org/3/movie/550?api_key=39c3e7befb37432f892ebd79b38ddfe6');
-                    const movie = await res.json();
-                    this.movie = movie;
+                    const res = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=39c3e7befb37432f892ebd79b38ddfe6');
+                    const movies = await res.json();
+                    
+                    this.movies = movies.results;
                 } catch(error) {
                     console.error(error);
                 }
